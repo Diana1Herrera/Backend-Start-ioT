cde1d3d7b68fd1e203bfe618bec6a3cf020bd2fd
Diana1Herrera <dianis11_3@hotmail.com>
2025-02-21 10:19:01 -0500

    Listo

--- src/main/java/com/StartIot/StartIot/WebConfig.java
+++ src/main/java/com/StartIot/StartIot/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5502/Proyecto-Start-ioT/Index/home.html") // Asegúrate de que sea una URL válida
+                .allowedOriginPatterns("http://127.0.0.1:5502") // Asegúrate de que sea una URL válida
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
+                //.allowCredentials(true);
     }
 }
